440514e72e637011a72dff798c21db4f06d52be9
shadowlerone <andrae.lewis@gmail.com>
2021-04-19 11:19:19 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Names
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lstNames.Items.Add("shadowlerone");
+        }
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+                lstNames.Items.Add(txtName.Text);
+        }
+
+        private void reloadFilePath(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(installPath.Text))
+            {
+                // This path is a file
+                ProcessFile(installPath.Text);
+            }
+            else if (Directory.Exists(installPath.Text))
+            {
+                // This path is a directory
+                ProcessDirectory(installPath.Text + "\\Mods");
+            }
+            else
+            {
+                debug.Text += ("{0} is not a valid file or directory.\n", installPath.Text);
+            }
+        }
+
+        private void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+        private void ProcessFile(string path)
+        {
+            debug.Text += ("Processed file '{0}'.\n", path);
+        }
+    }
+}
